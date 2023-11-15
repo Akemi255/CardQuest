19291e29800af8f09b361984ca93895c52d35778
gusvet18 <gus1465@yahoo.com>
2023-11-15 09:27:59 -0500

    Botón de eliminar cartas implementado

--- src/components/SavedCards/SavedCardsSection.jsx
+++ src/components/SavedCards/SavedCardsSection.jsx
@@ -4,15 +4,16 @@ import { BiSolidTrashAlt } from "react-icons/bi";
 import { ProfileSavedCards } from "./ProfileSavedCards";
 import Footer from "../Layout/footer";
 import { getEmail } from "@/helpers/getEmail";
-
+import Modal from "react-modal";
 
 const SavedCardsSection = () => {
   const [userCards, setUserCards] = useState([]);
   const [visibleCards, setVisibleCards] = useState(10);
   const [searchTerm, setSearchTerm] = useState("");
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const userEmail = getEmail();
- 
+
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -64,12 +65,55 @@ const SavedCardsSection = () => {
     };
   }, []);
 
-  const filteredCards = userCards.filter((card) =>
-  card.content.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
-  card.content.anime.some(
-    (anime) => anime.anime.title.toLowerCase().includes(searchTerm.toLowerCase())
-  )
-);
+  const filteredCards = userCards.filter(
+    (card) =>
+      card.content.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      card.content.anime.some((anime) =>
+        anime.anime.title.toLowerCase().includes(searchTerm.toLowerCase())
+      )
+  );
+
+  const handleDeleteCards = async () => {
+    // Abre el modal al hacer clic en "Borrar cartas"
+    setIsModalOpen(true);
+  };
+
+  const confirmDelete = async () => {
+    // Cerrar el modal
+    setIsModalOpen(false);
+
+    try {
+      const response = await fetch(
+        "http://localhost:3002/api/cards/deleteCards",
+        {
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email: userEmail,
+          }),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error(
+          "Error al realizar la petición de eliminación de cartas"
+        );
+      }
+      
+      console.log("peticion delete ejecutada correctamente");
+      // Actualiza el estado para reflejar que las cartas han sido eliminadas
+      setUserCards([]);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  const cancelDelete = () => {
+    // Cierra el modal sin realizar la eliminación
+    setIsModalOpen(false);
+  };
 
   return (
     <>
@@ -90,16 +134,50 @@ const SavedCardsSection = () => {
               <FiSearch size={25} />
             </button>
           </div>
-          <h2 className="flex items-center mr-2 text-2xl font-bold text-white cursor-pointer hover:text-red-700 relative transition duration-300 ease-in-out">
+          <h2
+            className="flex items-center mr-2 text-2xl font-bold text-white cursor-pointer hover:text-red-700 relative transition duration-300 ease-in-out"
+            onClick={handleDeleteCards}
+          >
             Borrar cartas <BiSolidTrashAlt size={32} color="red" />
           </h2>
         </div>
         <div className="flex flex-wrap mt-5 ">
           {filteredCards.slice(0, visibleCards).map((card, index) => (
-            <ProfileSavedCards key={index} character={card.content} index={index} />
+            <ProfileSavedCards
+              key={index}
+              character={card.content}
+              index={index}
+            />
           ))}
         </div>
       </div>
+      {/* Modal */}
+      
+      <Modal
+        isOpen={isModalOpen}
+        onRequestClose={cancelDelete}
+        contentLabel="Confirmar eliminación"
+        className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-slate-800 rounded-md p-4 text-white w-80"
+        overlayClassName="fixed inset-0"
+        ariaHideApp={false}
+      >
+        <h2 className="text-xl font-bold mb-4">
+          ¿Está seguro que desea eliminar todas sus cartas?
+        </h2>
+        <p className="mb-4">Esta acción no tiene vuelta atrás.</p>
+        <button
+          className="bg-red-500 text-white px-4 py-2 rounded mr-2"
+          onClick={confirmDelete}
+        >
+          Sí
+        </button>
+        <button
+          className="bg-slate-800 text-white px-4 py-2 rounded"
+          onClick={cancelDelete}
+        >
+          Cancelar
+        </button>
+      </Modal>
     </>
   );
 };
