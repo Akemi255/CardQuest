d5eb0989bda86e6f2ba122e412144982ca9da073
alexislan <alexislanchiotti13@gmail.com>
2024-01-27 15:59:47 -0300

    arreglo de bug en singup

--- src/app/sign-up/[[...sign-up]]/page.jsx
+++ src/app/sign-up/[[...sign-up]]/page.jsx
@@ -1,11 +1,14 @@
 import { SignUp } from "@clerk/nextjs";
- 
+import '/public/css/singUp.css'
+
 export default function Page() {
   return(
     <>
-      <div className="prueba">
+      <div className="card-singUp">
+        <img src="/assets/forms-barra.png" alt="Gema-form" className="gema"/>
+      <p className="tituloSingUp">Crea tu cuenta</p>
 
-        <SignUp />
+        <SignUp/>
       </div>
     </>
   )
