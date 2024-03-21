b446db7fb8d86b9346717454f18756cd825d66a3
Arrofi Reza Satria <arrofirezasatria@gmail.com>
2024-03-21 09:55:23 +0700

    update

--- src/app/(profile)/ranking/page.jsx
+++ src/app/(profile)/ranking/page.jsx
@@ -10,12 +10,14 @@ import {
   TableRow,
 } from "@/components/ui/table";
 
+import UserTable from "./components/UserTable";
+
 export default function page() {
   return (
     <div className="bg-[#171928] h-screen">
       <div className="container">
-        <Table className="text-white">
-          {/* <TableCaption>A list of your recent invoices.</TableCaption> */}
+        <Table className="text-white [&_.text-muted-foreground]:text-white">
+          <TableCaption>User Ranking</TableCaption>
           <TableHeader>
             <TableRow>
               <TableHead className="w-[100px]">Ranking</TableHead>
@@ -25,12 +27,22 @@ export default function page() {
             </TableRow>
           </TableHeader>
           <TableBody>
-            <TableRow>
-              <TableCell className="font-medium">1</TableCell>
-              <TableCell>John Doe</TableCell>
-              <TableCell>234</TableCell>
-              <TableCell className="text-right">234</TableCell>
-            </TableRow>
+            {Array.from(Array(10).keys()).map((user, index) => {
+              return (
+                <TableRow key={index}>
+                  <TableCell className="font-medium">{index + 1}</TableCell>
+                  <TableCell>
+                    <UserTable
+                      username={"John Doe"}
+                      userUrl={"https://www.google.com"}
+                      imageUrl={"https://github.com/shadcn.png"}
+                    />
+                  </TableCell>
+                  <TableCell>234</TableCell>
+                  <TableCell className="text-right">234</TableCell>
+                </TableRow>
+              );
+            })}
           </TableBody>
         </Table>
       </div>
