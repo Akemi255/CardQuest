1472159aed1c0e1b0baa2a4df811ad0dfecc6734
Arrofi Reza Satria <arrofirezasatria@gmail.com>
2024-03-21 16:37:23 +0700

    update

--- src/app/(profile)/ranking/data/fetchRanking.js
+++ src/app/(profile)/ranking/data/fetchRanking.js
@@ -1,3 +1,22 @@
 export default async function fetchRanking() {
-  return;
+  const res = async () => {
+    try {
+      const response = await fetch(
+        "https://api-rest-card-quest.vercel.app/api/users/getUsersRank"
+      );
+
+      if (!response.ok) {
+        throw new Error("Error al obtener los datos del usuario");
+      }
+
+      const data = await response.json();
+      // setUsersData(data);
+      console.log(usersData);
+      return data;
+    } catch (error) {
+      console.error("Error al obtener los datos del usuario:", error);
+    }
+  };
+
+  return res;
 }
