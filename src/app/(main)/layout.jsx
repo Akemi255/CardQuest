81b3d2b414e067328e1a88299fa4285a6d446aec
gusvet18 <gus1465@yahoo.com>
2024-04-03 20:59:14 -0500

    hidration error fixed

--- src/app/(main)/layout.jsx
+++ src/app/(main)/layout.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import MyDrawer from "./components/MyDrawer";
 import Nav from "./components/Nav";
 
@@ -8,18 +8,28 @@ import SearchBar from "./components/SearchBar";
 import TittleBar from "./components/TittleBar";
 
 export default function layout({ children }) {
+  const [mounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
   return (
-    <div className="grid flex-1 md:grid-cols-[240px_1fr] h-screen">
-      <aside className="hidden md:flex w-[240px] flex-col bg-background2 border-r border-[#2E2E2E]">
-        <Nav />
-      </aside>
-      <main className="flex w-full flex-1 flex-col overflow-hidden bg-background2  h-max md:h-auto">
-        <SearchBar />
-        {/* <TittleBar /> */}
-        {/* <div className="max-w-7xl mx-auto mt-20 overflow-y-scroll"> */}
-        <div className="overflow-y-scroll">{children}</div>
-      </main>
-      <MyDrawer />
-    </div>
+    <>
+      {mounted && (
+        <div className="grid flex-1 md:grid-cols-[240px_1fr] h-screen">
+          <aside className="hidden md:flex w-[240px] flex-col bg-background2 border-r border-[#2E2E2E]">
+            <Nav />
+          </aside>
+          <main className="flex w-full flex-1 flex-col overflow-hidden bg-background2  h-max md:h-auto">
+            <SearchBar />
+            {/* <TittleBar /> */}
+            {/* <div className="max-w-7xl mx-auto mt-20 overflow-y-scroll"> */}
+            <div className="overflow-y-scroll">{children}</div>
+          </main>
+          <MyDrawer />
+        </div>
+      )}
+    </>
   );
 }
