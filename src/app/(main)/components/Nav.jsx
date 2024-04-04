378c67b626c1c3cab7fe25c8e18fb63cdbff80bb
rofireza <arrofirezasatria@gmail.com>
2024-04-04 23:18:02 +0700

    update

--- src/app/(main)/components/Nav.jsx
+++ src/app/(main)/components/Nav.jsx
@@ -245,27 +245,12 @@ export default function Nav() {
         {/* <Separator className="bg-[#2E2E2E]" /> */}
       </nav>
 
-      {/* <Link href={"/Settings"} className="m-3">
-        <span
-          className={cn(
-            "group flex items-center rounded-sm text-sm font-medium hover:bg-background-surface-200 hover:text-primary-foreground-morelighter  text-primary-foreground-light transition-all",
-            path === "/Settings"
-              ? "bg-background-surface-200 text-primary-foreground-morelighter"
-              : "transparent",
-            false && "cursor-not-allowed opacity-80"
-          )}
-        >
-          <span className="h-10 w-10 flex items-center justify-center rounded">
-            <Settings className="h-5 w-5" />
-          </span>
-          <span className="text-[13px]">Settings</span>
-        </span>
-      </Link> */}
+      <div className="hidden md:flex"></div>
       <Popover>
         <PopoverTrigger asChild>
           <Button
             variant="outline"
-            className="hover:bg-background-surface-200 bg-background2 border-border-button w-auto flex flex-row justify-start h-auto p-0 text-primary-foreground-light hover:text-primary-foreground-morelighter border mx-2 mb-2"
+            className="hover:bg-background-surface-200 bg-background2 border-border-button w-auto flex md:hidden flex-row justify-start h-auto p-0 text-primary-foreground-light hover:text-primary-foreground-morelighter border mx-2 mb-2 "
           >
             <div className="flex h-10 w-10  justify-center items-center">
               <Avatar className="h-7 w-7 ">
@@ -346,40 +331,6 @@ export default function Nav() {
           </Button>
         </PopoverContent>
       </Popover>
-      {/* <Popover>
-        <PopoverTrigger asChild>
-          <Button
-            variant="outline"
-            className="hover:bg-background-surface-200 bg-background2 border-border-button m-3 flex flex-row justify-start h-auto p-0 text-primary-foreground-light hover:text-primary-foreground-morelighter border "
-          >
-            <div className="flex h-10 w-10 justify-center items-center">
-              <Avatar className="h-7 w-7 ">
-                <AvatarImage
-                  src="https://github.com/shadcn.png"
-                  alt="@shadcn"
-                />
-                <AvatarFallback>CN</AvatarFallback>
-              </Avatar>
-            </div>
-            <div className="flex-col flex w-auto text-start">
-              <p className="text-[13px] ">John Doe</p>
-              <p className="text-xs text-clip overflow-hidden">
-                arrofirezasatria@gmail.com
-              </p>
-            </div>
-          </Button>
-        </PopoverTrigger>
-        <PopoverContent className="w-80">
-          <div className="grid gap-4">
-            <div className="space-y-2">
-              <h4 className="font-medium leading-none">Dimensions</h4>
-              <p className="text-sm text-muted-foreground">
-                Set the dimensions for the layer.
-              </p>
-            </div>
-          </div>
-        </PopoverContent>
-      </Popover> */}
     </div>
   );
 }
