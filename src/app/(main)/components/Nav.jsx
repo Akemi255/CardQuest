5b2bb0c02a94fa5b67d3aa4b616ea4d46a4a42d3
gusvet18 <gus1465@yahoo.com>
2024-04-15 21:49:37 -0500

    path for user fixed

--- src/app/(main)/components/Nav.jsx
+++ src/app/(main)/components/Nav.jsx
@@ -103,16 +103,6 @@ const Links = [
       },
     ],
   },
-
-  // {
-  //   title: "Profile",
-  //   href: "/store",
-  //   label: "128",
-  //   icon: Store,
-  //   variant: "default",
-  //   separator: false,
-  //   subMenu: null,
-  // },
 ];
 
 export default function Nav() {
@@ -133,7 +123,7 @@ export default function Nav() {
       <nav className="grid items-start gap-y-2 p-3">
         {Links.map((link, index) => {
           return (
-            <>
+            <React.Fragment key={index}>
               {link.subMenu ? (
                 <>
                   {link.separator && <Separator className="bg-[#2E2E2E]" />}
@@ -143,9 +133,9 @@ export default function Nav() {
                   >
                     <span
                       className={cn(
-                        "group flex items-center rounded-sm text-sm font-medium hover:bg-background-surface-200 hover:text-primary-foreground-morelighter  text-primary-foreground-light transition-all",
+                        "group flex items-center rounded-sm text-sm font-medium hover:bg-background-surface-200 hover:text-primary-foreground-morelighter text-primary-foreground-light transition-all",
                         path === "settings"
-                          ? "bg-background-surface-200 "
+                          ? "bg-background-surface-200"
                           : "transparent",
                         false && "cursor-not-allowed opacity-80"
                       )}
@@ -167,7 +157,7 @@ export default function Nav() {
                           >
                             <span
                               className={cn(
-                                "group flex items-center rounded-sm text-sm font-medium hover:bg-background-surface-200 hover:text-primary-foreground-morelighter  text-primary-foreground-light transition-all",
+                                "group flex items-center rounded-sm text-sm font-medium hover:bg-background-surface-200 hover:text-primary-foreground-morelighter text-primary-foreground-light transition-all",
                                 path === "settings"
                                   ? "bg-background-surface-200"
                                   : "transparent",
@@ -188,11 +178,14 @@ export default function Nav() {
                   )}
                 </>
               ) : (
-                <Link key={index} href={link.href}>
+                <Link href={link.href} key={index}>
                   <span
                     className={cn(
-                      "group flex items-center rounded-sm text-sm font-medium hover:bg-background-surface-200 hover:text-primary-foreground-morelighter  text-primary-foreground-light transition-all",
-                      path === link.href
+                      "group flex items-center rounded-sm text-sm font-medium hover:bg-background-surface-200 hover:text-primary-foreground-morelighter text-primary-foreground-light transition-all",
+                      path.startsWith("/users/") &&
+                        link.href.startsWith("/users/")
+                        ? "bg-background-surface-200 text-primary-foreground-morelighter"
+                        : path === link.href
                         ? "bg-background-surface-200 text-primary-foreground-morelighter"
                         : "transparent",
                       false && "cursor-not-allowed opacity-80"
@@ -205,62 +198,10 @@ export default function Nav() {
                   </span>
                 </Link>
               )}
-            </>
+            </React.Fragment>
           );
         })}
       </nav>
-      {/* 
-      <Link href={"/Settings"} className="m-3">
-        <span
-          className={cn(
-            "group flex items-center rounded-sm text-sm font-medium hover:bg-background-surface-200 hover:text-primary-foreground-morelighter  text-primary-foreground-light transition-all",
-            path === "/Settings"
-              ? "bg-background-surface-200 text-primary-foreground-morelighter"
-              : "transparent",
-            false && "cursor-not-allowed opacity-80"
-          )}
-        >
-          <span className="h-10 w-10 flex items-center justify-center rounded">
-            <Settings className="h-5 w-5" />
-          </span>
-          <span className="text-[13px]">Settings</span>
-        </span>
-      </Link> */}
-
-      {/* <Popover>
-        <PopoverTrigger asChild>
-          <Button
-            variant="outline"
-            className="hover:bg-background-surface-200 bg-background2 border-border-button m-3 flex flex-row justify-start h-auto p-0 text-primary-foreground-light hover:text-primary-foreground-morelighter border "
-          >
-            <div className="flex h-10 w-10 justify-center items-center">
-              <Avatar className="h-7 w-7 ">
-                <AvatarImage
-                  src="https://github.com/shadcn.png"
-                  alt="@shadcn"
-                />
-                <AvatarFallback>CN</AvatarFallback>
-              </Avatar>
-            </div>
-            <div className="flex-col flex w-auto text-start">
-              <p className="text-[13px] ">John Doe</p>
-              <p className="text-xs text-clip overflow-hidden">
-                arrofirezasatria@gmail.com
-              </p>
-            </div>
-          </Button>
-        </PopoverTrigger>
-        <PopoverContent className="w-80">
-          <div className="grid gap-4">
-            <div className="space-y-2">
-              <h4 className="font-medium leading-none">Dimensions</h4>
-              <p className="text-sm text-muted-foreground">
-                Set the dimensions for the layer.
-              </p>
-            </div>
-          </div>
-        </PopoverContent>
-      </Popover> */}
     </div>
   );
 }
