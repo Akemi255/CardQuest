9727a652954083feb9d62b68699497f3078645b0
Arrofi Reza Satria <arrofirezasatria@gmail.com>
2024-04-20 13:02:00 +0700

    update

--- src/app/(main)/users/user/[user]/page.jsx
+++ src/app/(main)/users/user/[user]/page.jsx
@@ -1,12 +1,15 @@
+"use client";
+
 import { notFound } from "next/navigation";
 
 import ExploreProfile from "./components/exploreProfile";
 import UserCards from "./components/userCards";
+import { Button } from "@/components/ui/button";
 
 const getData = async (userId) => {
   try {
     const response = await fetch(
-      `https://api-rest-card-quest.vercel.app/api/cards/findUserCardsById/${userId}`,
+      `https://api-rest-card-quest.vercel.app/api/apiCards/sortedByCoins/1`,
       { cache: "no-store" }
     );
     const data = await response.json();
@@ -22,7 +25,9 @@ const page = async ({ params }) => {
   return (
     <>
       <ExploreProfile />
-      <UserCards data={data} />
+      {/* <UserCards data={data} /> */}
+      <Button>next</Button>
+      <Button>prev</Button>
     </>
   );
 };
