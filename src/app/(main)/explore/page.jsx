ba9b809709f232d109e4a15f5e816daa80c16bc8
Arrofi Reza Satria <arrofirezasatria@gmail.com>
2024-04-22 14:09:04 +0700

    update

--- src/app/(main)/explore/page.jsx
+++ src/app/(main)/explore/page.jsx
@@ -1,46 +1,64 @@
 "use client";
 
 import { Input } from "@/components/ui/input";
-import React, { Suspense, useState } from "react";
-import { usePathname, useSearchParams, useRouter } from "next/navigation";
-import Search from "./components/Search";
-import List from "./components/List";
-import PaginationComponent from "./components/PaginationComponent";
+import React, { useState } from "react";
 import useSWR from "swr";
-import RenderUserCards from "@/components/Cards/RenderUserCards";
 import RenderExploreCards from "./components/RenderExploreCards";
+
+import { useDebouncedCallback } from "use-debounce";
 
 const fetcher = async (...args) =>
   await fetch(...args).then((res) => res.json());
 
 export default function page() {
   const [pageIndex, setPageIndex] = useState(1);
+  const [searchTerm, setSearchTerm] = useState("");
 
   const { data, isLoading } = useSWR(
-    `https://api-rest-card-quest.vercel.app/api/apiCards/sortedByCoins/1`,
+    `https://api-rest-card-quest.vercel.app/api/apiCards/sortedByCoins/${pageIndex}`,
     fetcher
   );
 
-  console.log(data);
+  //TODO:
+  const filteredCards = data?.cards?.filter((card) =>
+    card?.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
-  // const query = searchParams?.query || "";
-  // const currentPage = Number(searchParams?.page) || 1;
+  // console.log(data);
 
   return (
     <div>
-      {/* <Search /> */}
-      {/* <List query={query} currentPage={currentPage} /> */}
-      {/* <PaginationComponent /> */}
+      <Input
+        type="text"
+        placeholder="Search by name"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        className="mt-3 px-2 py-1 mr-3 rounded-md border border-gray-300 w-full sm:w-1/2"
+      />
       <div className="flex flex-col items-center">
         <div className="flex flex-wrap gap-[20px] justify-center mt-7 mb-[50px]">
-          {data?.cards?.map((card, index) => (
-            <RenderExploreCards key={index} index={index} character={card} />
-          ))}
+          {isLoading ? (
+            <div className="text-white">Loading,...</div>
+          ) : (
+            filteredCards?.map((card, index) => (
+              <RenderExploreCards key={index} index={index} character={card} />
+            ))
+          )}
         </div>
       </div>
 
-      <button onClick={() => setPageIndex(pageIndex - 1)}>Previous</button>
-      <button onClick={() => setPageIndex(pageIndex + 1)}>Next</button>
+      <button
+        onClick={() => setPageIndex(pageIndex - 1)}
+        className="text-white"
+      >
+        Previous
+      </button>
+      <button
+        onClick={() => setPageIndex(pageIndex + 1)}
+        className="text-white"
+      >
+        Next
+      </button>
     </div>
   );
 }
